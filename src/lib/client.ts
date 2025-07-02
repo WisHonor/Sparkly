6e57e142ea76145b4b3d09d1dc4e15efa810281f
Wissam Elmasry <elmasry.wissam@gmail.com>
2025-07-02 01:19:00 -0400

    commit2

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,8 @@
 import { AppType } from "@/server"
 import { hc } from "hono/client"
 import { HTTPException } from "hono/http-exception"
-import { StatusCode } from "hono/utils/http-status"
+
+
 import superjson from "superjson"
 
 const getBaseUrl = () => {
@@ -22,7 +23,7 @@ export const baseClient = hc<AppType>(getBaseUrl(), {
     const response = await fetch(input, { ...init, cache: "no-store" })
 
     if (!response.ok) {
-      throw new HTTPException(response.status as StatusCode, {
+      throw new HTTPException(response.status as any, {
         message: response.statusText,
         res: response,
       })
